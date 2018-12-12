5be37c344142705886ec6815d93a2c5c25c5cb66
gerkenv <gerkenv@gmail.com>
2018-12-12 22:45:33 +0100

    + stack should be implemented;

---
+++ src/data-structures/Stack.ts
@@ -0,0 +1,16 @@
+import { Node } from './LinkedList';
+
+/**
+ * Stack based on singly linked list
+ */
+export class StackOnLinkedList {
+  private _first: Node;
+  /**
+   * Create a new stack.
+   */
+  constructor() {
+    this._first = null;
+  }
+
+}
+
