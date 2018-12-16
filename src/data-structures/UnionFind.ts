28b1dabe81a49925e056c96708258a0fb578956b
gerkenv <gerkenv@gmail.com>
2018-12-16 23:44:54 +0100

    + Weighted QFUF; + WQFUF with path compression;

--- src/data-structures/UnionFind.ts
+++ src/data-structures/UnionFind.ts
@@ -18,24 +18,35 @@
  * - `connected` - constant time.
  */
 export class UnionFindQuickFind {
-  private _array: number[];
+  private _nodes: number[];
   /**
    * Create a union-find structure.
    * @param size - quantity of elements in a structure.
    */
   constructor(size : number) {
     for (let i : number = 0; i < size; i++) {
-      this._array[i] = i;
+      this._nodes[i] = i;
     }
   }
 
   /**
    * Find out to which group belongs element.
    * @param element - element to search.
-   * @returns - union / component unique identifier.
+   * @returns - union / component unique identifier / root element.
    */
   find = (element : number) : number => {
-    return this._array[element];
+    this.validateIndex(element);
+    return this._nodes[element];
+  }
+
+  /**
+   * Check index boundaries
+   * @param element - element to search.
+   */
+  private validateIndex = (element : number) : void => {
+    if ((element < 0) || (element >= this._nodes.length)) {
+      throw `Index ${element} is out of valid range [0, ${this._nodes.length}]`;
+    }
   }
 
   /**
@@ -45,7 +56,7 @@ export class UnionFindQuickFind {
    * returns - `true` if elements belong to the same group.
    */
   connected = (elementA : number, elementB : number) : boolean => {
-    return this._array[elementA] === this._array[elementB];
+    return this.find(elementA) === this.find(elementB);
   }
 
   /**
@@ -54,10 +65,10 @@ export class UnionFindQuickFind {
    * @param elementB - another element of structure.
    */
   union = (elementA : number, elementB : number) : void => {
-    let idA : number = this._array[elementA];
-    let idB : number = this._array[elementB];
-    for (let i : number = 0; i < this._array.length; i++) {
-      if (this._array[i] == idA) this._array[i] = idB;
+    let idA : number = this.find(elementA);
+    let idB : number = this.find(elementB);
+    for (let i : number = 0; i < this._nodes.length; i++) {
+      if (this._nodes[i] == idA) this._nodes[i] = idB;
     }
   }
 }
@@ -83,34 +94,36 @@ export class UnionFindQuickFind {
  * - `connected` - linear time.
  */
 export class UnionFindQuickUnion {
-  private _array: number[];
+  private _nodes: number[];
   /**
    * Create a union-find structure.
    * @param size - quantity of elements in a structure.
    */
   constructor(size : number) {
     for (let i : number = 0; i < size; i++) {
-      this._array[i] = i;
+      this._nodes[i] = i;
     }
   }
 
   /**
    * Find out to which group belongs element.
    * @param element - element to search.
-   * @returns - union / component unique identifier.
+   * @returns - union / component unique identifier / root element.
    */
   find = (element : number) : number => {
-    return this.root(element);
+    this.validateIndex(element);
+    while (element !== this._nodes[element]) element = this._nodes[element];
+    return element;
   }
 
   /**
-   * Get group root
-   * @param element - element to examine.
-   * @returns - union / component unique identifier.
+   * Check index boundaries
+   * @param element - element to search.
    */
-  private root = (element : number) : number => {
-    while (element !== this._array[element]) element = this._array[element];
-    return element;
+  private validateIndex = (element : number) : void => {
+    if ((element < 0) || (element >= this._nodes.length)) {
+      throw `Index ${element} is out of valid range [0, ${this._nodes.length}]`;
+    }
   }
 
   /**
@@ -120,7 +133,7 @@ export class UnionFindQuickUnion {
    * returns - `true` if elements belong to the same group.
    */
   connected = (elementA : number, elementB : number) : boolean => {
-    return this.root(elementA) === this.root(elementB);
+    return this.find(elementA) === this.find(elementB);
   }
 
   /**
@@ -129,8 +142,179 @@ export class UnionFindQuickUnion {
    * @param elementB - another element of structure.
    */
   union = (elementA : number, elementB : number) : void => {
-    let idA : number = this.root(elementA);
-    let idB : number = this.root(elementB);
-    this._array[idA] = idB;
+    let idA : number = this.find(elementA);
+    let idB : number = this.find(elementB);
+    this._nodes[idA] = idB;
+  }
+}
+
+
+/**
+ * Weighted union-find class with quick `union`.
+ * Class represents 'Union-find' data type also known as 'disjoint data set'.
+ *
+ * The union–find data type models connectivity among a set of `n`
+ * elements, named `0` through `n–1`.
+ * All elements that are connected with each other represent one group.
+ *
+ * Initially each element represents its own group.
+ * - `union` operation combines groups together.
+ * - `find` operation returns a unique group identifier (group root).
+ * - `connected` operation returns `true` if elements belong to the same group.
+ *
+ * Time complexity:
+ * - Initialization - linear time.
+ * - `union` - logarithmic time.
+ * - `find` - logarithmic time.
+ * - `connected` - logarithmic time.
+ */
+export class WeightedUnionFindQuickUnion {
+  private _nodes: number[];
+  private _size: number[];
+
+  /**
+   * Create a union-find structure.
+   * @param size - quantity of elements in a structure.
+   */
+  constructor(size : number) {
+    for (let i : number = 0; i < size; i++) {
+      this._nodes[i] = i;
+      this._size[i] = 1;
+    }
+  }
+
+  /**
+   * Find out to which group belongs element.
+   * @param element - element to search.
+   * @returns - union / component unique identifier / root element.
+   */
+  find = (element : number) : number => {
+    this.validateIndex(element);
+    while (element !== this._nodes[element]) element = this._nodes[element];
+    return element;
+  }
+
+  /**
+   * Check index boundaries
+   * @param element - element to search.
+   */
+  private validateIndex = (element : number) : void => {
+    if ((element < 0) || (element >= this._nodes.length)) {
+      throw `Index ${element} is out of valid range [0, ${this._nodes.length}]`;
+    }
+  }
+
+  /**
+   * Check if two elements are connected.
+   * @param elementA - some element of structure.
+   * @param elementB - another element of structure.
+   * returns - `true` if elements belong to the same group.
+   */
+  connected = (elementA : number, elementB : number) : boolean => {
+    return this.find(elementA) === this.find(elementB);
+  }
+
+  /**
+   * Unite groups of 2 elements together.
+   * @param elementA - some element of structure.
+   * @param elementB - another element of structure.
+   */
+  union = (elementA : number, elementB : number) : void => {
+    let idA : number = this.find(elementA);
+    let idB : number = this.find(elementB);
+    if (idA === idB) return;
+    // Pack smaller tree under the bigger tree of nodes
+    if (this._size[idA] < this._size[idB]) {
+      this._nodes[idA] = idB; this._size[idB] += this._size[idA];
+    } else {
+      this._nodes[idB] = idA; this._size[idA] += this._size[idB];
+    }
+  }
+}
+
+
+/**
+ * Weighted union-find class with quick `union`.
+ * Class represents 'Union-find' data type also known as 'disjoint data set'.
+ *
+ * The union–find data type models connectivity among a set of `n`
+ * elements, named `0` through `n–1`.
+ * All elements that are connected with each other represent one group.
+ *
+ * Initially each element represents its own group.
+ * - `union` operation combines groups together.
+ * - `find` operation returns a unique group identifier (group root).
+ * - `connected` operation returns `true` if elements belong to the same group.
+ *
+ * Time complexity:
+ * - Initialization - linear time.
+ * - `union` - logarithmic time.
+ * - `find` - logarithmic time.
+ * - `connected` - logarithmic time.
+ */
+export class WeightedUnionFindQuickUnionWithPathCompressions {
+  private _nodes: number[];
+  private _size: number[];
+
+  /**
+   * Create a union-find structure.
+   * @param size - quantity of elements in a structure.
+   */
+  constructor(size : number) {
+    for (let i : number = 0; i < size; i++) {
+      this._nodes[i] = i;
+      this._size[i] = 1;
+    }
+  }
+
+  /**
+   * Find out to which group belongs element.
+   * @param element - element to search.
+   * @returns - union / component unique identifier / root element.
+   */
+  find = (element : number) : number => {
+    this.validateIndex(element);
+    while (element !== this._nodes[element]) {
+      this._nodes[element] = this._nodes[this._nodes[element]];
+      element = this._nodes[element];
+    }
+    return element;
+  }
+
+  /**
+   * Check index boundaries
+   * @param element - element to search.
+   */
+  private validateIndex = (element : number) : void => {
+    if ((element < 0) || (element >= this._nodes.length)) {
+      throw `Index ${element} is out of valid range [0, ${this._nodes.length}]`;
+    }
+  }
+
+  /**
+   * Check if two elements are connected.
+   * @param elementA - some element of structure.
+   * @param elementB - another element of structure.
+   * returns - `true` if elements belong to the same group.
+   */
+  connected = (elementA : number, elementB : number) : boolean => {
+    return this.find(elementA) === this.find(elementB);
+  }
+
+  /**
+   * Unite groups of 2 elements together.
+   * @param elementA - some element of structure.
+   * @param elementB - another element of structure.
+   */
+  union = (elementA : number, elementB : number) : void => {
+    let idA : number = this.find(elementA);
+    let idB : number = this.find(elementB);
+    if (idA === idB) return;
+    // Pack smaller tree under the bigger tree of nodes
+    if (this._size[idA] < this._size[idB]) {
+      this._nodes[idA] = idB; this._size[idB] += this._size[idA];
+    } else {
+      this._nodes[idB] = idA; this._size[idA] += this._size[idB];
+    }
   }
 }
