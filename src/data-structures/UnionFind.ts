712bc9d7bc99e30e3d49f547085d4a2bafb935e9
gerkenv <gerkenv@gmail.com>
2018-12-16 14:05:25 +0100

    + description; + `find`;

--- src/data-structures/UnionFind.ts
+++ src/data-structures/UnionFind.ts
@@ -1,37 +1,71 @@
 /**
- * Union-find class with quick `find` function.
+ * Union-find class with quick `find`.
+ * Class represents 'Union-find' data type also known as 'disjoint data set'.
+ *
+ * The union–find data type models connectivity among a set of `n`
+ * elements, named `0` through `n–1`.
+ * All elements that are connected with each other represent one group.
+ *
+ * Initially each element represents its own group.
+ * - `union` operation combines groups together.
+ * - `find` operation returns a unique group identifier (group root).
+ * - `connected` operation returns `true` if elements belong to the same group.
+ *
+ * Functions `find` & `connected` take constant time.
+ * Initialization & `union` take linear time.
  */
 export class UnionFindQuickFind {
   private _array: number[];
   /**
    * Create a union-find structure.
-   * @param n - quantity of elements in a structure.
+   * @param size - quantity of elements in a structure.
    */
-  constructor(n : number) {
+  constructor(size : number) {
     for (let i : number = 0; i < n; n++) {
       this._array[i] = i;
     }
   }
 
   /**
-   * Check if two structure elements are connected.
-   * @param p - certain element of structure.
-   * @param q - certain element of structure.
+   * Find out to which group belongs element.
+   * @param element - element to search.
+   * @returns - union / component unique identifier.
    */
-  connect = (p : number, q : number) : boolean => {
-    return this._array[p] === this._array[q];
+  find = (element : number) : number => {
+    this.validateIndex(element);
+    return this._array[element];
   }
 
   /**
-   * Connect two elements in a structure.
-   * @param p - certain element of a structure.
-   * @param q - certain element of a structure.
+   * Check index boundaries
+   * @param element - element to search.
    */
-  union = (p : number, q : number) : void => {
-    let pid : number = this._array[p];
-    let qid : number = this._array[q];
+  private validateIndex = (element : number) : void => {
+    if ((element < 0) || (element >= this._array.length)) {
+      throw `Index ${element} is out of valid range`
+    }
+  }
+
+  /**
+   * Check if two elements are connected.
+   * @param elementA - some element of structure.
+   * @param elementB - another element of structure.
+   * returns - `true` if elements belong to the same group.
+   */
+  connected = (elementA : number, elementB : number) : boolean => {
+    return this._array[elementA] === this._array[elementB];
+  }
+
+  /**
+   * Unite groups of 2 elements together.
+   * @param elementA - some element of structure.
+   * @param elementB - another element of structure.
+   */
+  union = (elementA : number, elementB : number) : void => {
+    let idA : number = this._array[elementA];
+    let idB : number = this._array[elementB];
     for (let i : number = 0; i < this._array.length; i++) {
-      if (this._array[i] == pid) this._array[i] = qid;
+      if (this._array[i] == idA) this._array[i] = idB;
     }
   }
 }
