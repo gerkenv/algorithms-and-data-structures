166981d699d3671cca27c2a1262900fbabb11ed4
gerkenv <gerkenv@gmail.com>
2018-12-23 21:34:31 +0100

    +/- rebased on available data structures;

--- src/data-structures/Queue.js
+++ src/data-structures/Queue.js
@@ -1,68 +1,23 @@
 const Node = require('../../dist/data-structures/LinkedList').Node;
 const ResizingArray = require('../../dist/data-structures/ResizingArray').ResizingArray;
+const SinglyLinkedList = require('../../dist/data-structures/LinkedList').SinglyLinkedList;
 
 /**
  * Queue collection (FIFO bag) based on nodes with single link.
  */
-class QueueOnLinkedList {
+class QueueOnSinglyLinkedList {
   /**
    * Create a new empty queue.
    */
   constructor() {
-    this._first = undefined;
-    this._last = undefined;
+    this._items = new SinglyLinkedList();
   }
-
   /**
-   * Check if a queue is empty.
-   * @returns `true` - queue is empty.
-   * @returns `false` - queue is not empty.
+   * Returns the number of element in a queue.
    */
-  isEmpty() {
-    return (this._first == undefined);
+  length() {
+    return this._items.length();
   }
-
-  /**
-   * Add an `item` to the end of a queue.
-   * @param {*} item - an `item` to add to the end of a queue.
-   * @returns nothing.
-   */
-  enqueue(item) {
-    let oldLast = this._last;
-    this._last = new Node(item);
-    if (this.isEmpty()) {
-      this._first = this._last;
-    } else {
-      oldLast.next = this._last;
-    }
-  }
-
-  /**
-   * Takes an `item` and removes it from a beginning of a queue.
-   * @returns {*} `item` - an item that was first in a queue.
-   */
-  dequeue() {
-    if (this.isEmpty()) return undefined;
-    let item = this._first.data;
-    this._first = this._first.next;
-    if (this.isEmpty()) this._last = undefined;
-    return item;
-  }
-}
-
-module.exports.QueueOnLinkedList = QueueOnLinkedList;
-
-/**
- * Queue collection (FIFO bag) based on nodes with single link.
- */
-class QueueOnArray {
-  /**
-   * Create a new empty queue.
-   */
-  constructor() {
-    this._items = new ResizingArray();
-  }
-
   /**
    * Add an `item` to the end of a queue.
    * @param {*} item - an `item` to add to the end of a queue.
@@ -71,14 +26,49 @@ class QueueOnArray {
   enqueue(item) {
     this._items.push(item);
   }
+  /**
+   * Takes an `item` and removes it from a beginning of a queue.
+   * @returns {*} `item` - an item that was first in a queue.
+   * @returns {undefined} - if a queue is empty.
+   */
+  dequeue() {
+    return this._items.shift();
+  }
+}
 
+module.exports.QueueOnSinglyLinkedList = QueueOnSinglyLinkedList;
+
+/**
+ * Queue collection (FIFO bag) based on nodes with single link.
+ */
+class QueueOnResizingArray {
+  /**
+   * Create a new empty queue.
+   */
+  constructor() {
+    this._items = new ResizingArray();
+  }
+  /**
+   * Returns the number of element in a queue.
+   */
+  length() {
+    return this._items.length();
+  }
+  /**
+   * Add an `item` to the end of a queue.
+   * @param {*} item - an `item` to add to the end of a queue.
+   * @returns nothing.
+   */
+  enqueue(item) {
+    this._items.push(item);
+  }
   /**
    * Takes an `item` and removes it from a beginning of a queue.
    * @returns {*} `item` - an item that was first in a queue.
    */
   dequeue() {
-    return this._items.unshift;
+    return this._items.shift();
   }
 }
 
-module.exports.QueueOnArray = QueueOnArray;
+module.exports.QueueOnResizingArray = QueueOnResizingArray;
