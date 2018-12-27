de5f4367dd0fbe7b0dd8abc1809f847e31a46aa6
gerkenv <gerkenv@gmail.com>
2018-12-27 23:30:00 +0100

    + test for stack were implemented;

--- test/data-structures/Stack.spec.js
+++ test/data-structures/Stack.spec.js
@@ -1,12 +1,54 @@
-const StackOnLinkedList
-  = require('../../dist/data-structures/Stack').StackOnLinkedList;
+const StackOnSinglyLinkedList
+  = require('../../dist/data-structures/Stack').StackOnSinglyLinkedList;
+const StackOnResizingArray
+  = require('../../dist/data-structures/Stack').StackOnResizingArray;
 const expect = require('chai').expect;
 
-describe('StackOnLinkedList', function() {
-  describe('StackOnLinkedList', () => {
-    it('constructor should create an instance of an object', () => {
-      const result = new StackOnLinkedList();
-      expect(result).to.be.an('object');
-    })
+describe('Stack', function() {
+  const stacks = [StackOnResizingArray, StackOnSinglyLinkedList];
+  const stackClassNames = ['StackOnResizingArray', 'StackOnSinglyLinkedList'];
+  stacks.forEach((Stack, index) => {
+    describe(stackClassNames[index], () => {
+      describe('#constructor', () => {
+        const stack = new Stack();
+        it('should create an instance of object', () => {
+          expect(stack).to.be.an('object');
+        });
+        it('should create an empty stack', () => {
+          expect(stack.length()).to.be.equal(0);
+        });
+      });
+      describe('#push', () => {
+        const stack = new Stack();
+        const returnedValue = stack.push(1);
+        it('should return `undefined`', () => {
+          expect(returnedValue).to.be.undefined;
+        });
+        it('should increment quantity of elements in stack', () => {
+          for (let i = 2; i < 100; i++) {
+            stack.push(i);
+            const result = stack.length();
+            expect(result).to.be.equal(i);
+          }
+        });
+      });
+      describe('#pop', () => {
+        const stack = new Stack();
+        it('should return `undefined` if stack is empty', () => {
+          const result = stack.pop();
+          expect(result).to.be.undefined;
+        });
+        it('should return items in reversed order they were inserted in a stack', () => {
+          for (let i = 0; i < 100; i++) {
+            stack.push(i);
+          }
+          for (let i = 0; i < 100; i++) {
+            const result = stack.pop();
+            expect(result).to.be.equal(99-i);
+          }
+        });
+      });
+    });
   });
 });
+
